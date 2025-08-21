d851b7a18befa277b4b98737543fec9c4a36f059
Ulug'bek <au8700832@gmail.com>
2025-08-21 08:21:54 +0500

    Saved

--- library/urls.py
+++ library/urls.py
@@ -5,7 +5,7 @@ BookDeleteApiView, BookUpdateApiViwe, BookCreateApiView, BookMixedApiView
 
 urlpatterns = [
     path('kitoblar/', BookListApiView.as_view()),  
-    path('kitob/<int:id>/', BookDetailApiView.as_view()),
+    path('kitob/<int:pk>/', BookDetailApiView.as_view()),
     path('kitob/delete/<int:pk>/', BookDeleteApiView.as_view()),
     path('kitob/update/<int:pk>/', BookUpdateApiViwe.as_view()),
     path('kitob/create/', BookCreateApiView.as_view()),
--- library/views.py
+++ library/views.py
@@ -2,18 +2,41 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Books
 from .serializers import BoockSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
-class BookListApiView(generics.ListAPIView):
-    queryset = Books.objects.all()
-    serializer_class = BoockSerializer
+# class BookListApiView(generics.ListAPIView):
+#     queryset = Books.objects.all()
+#     serializer_class = BoockSerializer
 
-class BookDetailApiView(generics.RetrieveAPIView):
-    queryset = Books.objects.all()
-    serializer_class = BoockSerializer
-    lookup_field = 'id'
+class BookListApiView(APIView):
+    def get(self, request):
+        books = Books.objects.all()
+        serializer_data = BoockSerializer(books, many=True).data
+        info = {
+            'status':'All books have been shown',
+            'books list':serializer_data
+        }
+        return Response(info, status=status.HTTP_200_OK)
 
+
+# class BookDetailApiView(generics.RetrieveAPIView):
+#     queryset = Books.objects.all()
+#     serializer_class = BoockSerializer
+#     lookup_field = 'id'
+
+class BookDetailApiView(APIView):
+    def get(self, request, pk):
+        book = Books.objects.get(id=pk)
+        serializer_data = BoockSerializer(book).data
+        info = {
+            'status':'Book taken successfully',
+            'book info':serializer_data
+        }
+        return Response(info)
 class BookDeleteApiView(generics.DestroyAPIView):
     queryset = Books.objects.all()
     serializer_class = BoockSerializer
@@ -22,9 +45,21 @@ class BookUpdateApiViwe(generics.UpdateAPIView):
     queryset = Books.objects.all()
     serializer_class = BoockSerializer
 
-class BookCreateApiView(generics.CreateAPIView):
-    queryset = Books.objects.all()
-    serializer_class = BoockSerializer
+# class BookCreateApiView(generics.CreateAPIView):
+#     queryset = Books.objects.all()
+#     serializer_class = BoockSerializer
+
+class BookCreateApiView(APIView):
+    def post(self, request):
+        data = request.data
+        serializer = BoockSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            info = {
+                'status':"This book is saved to database",
+                'book info':data
+            }
+            return Response(info)
 
 class BookMixedApiView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Books.objects.all()
