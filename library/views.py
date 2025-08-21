4eb858df01404ee12fbdb475dc1b8dca01f5d019
Ulug'bek <au8700832@gmail.com>
2025-08-21 17:14:33 +0500

    Saved

--- library/views.py
+++ library/views.py
@@ -5,6 +5,7 @@ from .serializers import BoockSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -37,13 +38,48 @@ class BookDetailApiView(APIView):
             'book info':serializer_data
         }
         return Response(info)
-class BookDeleteApiView(generics.DestroyAPIView):
-    queryset = Books.objects.all()
-    serializer_class = BoockSerializer
+# class BookDeleteApiView(generics.DestroyAPIView):
+#     queryset = Books.objects.all()
+#     serializer_class = BoockSerializer
 
-class BookUpdateApiViwe(generics.UpdateAPIView):
-    queryset = Books.objects.all()
-    serializer_class = BoockSerializer
+# class BookDeleteApiView(APIView):
+    # def delete(self, request, pk):
+    #     try:
+    #         book = Books.objects.get(id=pk)
+    #         book.delete()
+    #         return Response({'status:''This book deleted succsessfully'})
+    #     except:
+    #         return Response({'status':'There is no book like that!'})
+class BookDeleteApiView(APIView):
+    def delete(self, request, pk):
+            book = get_object_or_404(Books, pk=pk)
+            book.delete()
+            return Response({'status':'This book deleted succsessfully'})
+
+
+# class BookUpdateApiViwe(generics.UpdateAPIView):
+#     queryset = Books.objects.all()
+#     serializer_class = BoockSerializer
+class BookUpdateApiViwe(APIView):
+    def put(self, request, pk):
+        book = get_object_or_404(Books, pk=pk)
+        serializer = BoockSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+        try:
+            book = Books.objects.get(id=pk)
+            serializer = BoockSerializer(book, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+        except:
+            return Response({"status":"Bad Request"}, status=status.HTTP_400_BAD_REQUEST)
+
 
 # class BookCreateApiView(generics.CreateAPIView):
 #     queryset = Books.objects.all()
@@ -61,6 +97,39 @@ class BookCreateApiView(APIView):
             }
             return Response(info)
 
-class BookMixedApiView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Books.objects.all()
-    serializer_class = BoockSerializer
+# class BookMixedApiView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Books.objects.all()
+#     serializer_class = BoockSerializer
+
+class BookMixedApiView(APIView):
+    def get(selfself, request, pk):
+           book = get_object_or_404(Books, pk=pk)
+           serializer = BoockSerializer(book).data
+           info = {
+               'status':'Query worked succesfully'
+           }
+           return Response(info)
+
+    def put(self, request, pk):
+        book = get_object_or_404(Books, pk=pk)
+        serializer = BoockSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response({"status: Your serializer has got a problem"})
+
+
+    def patch(self, request, pk):
+        book = get_object_or_404(Books, pk=pk)
+        serializer = BoockSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response({"status: Your serializer has got a problem"})
+
+    def delete(self, request, pk):
+        book = get_object_or_404(Books, pk=pk)
+        book.delete()
+        return Response({"status":"This book deleted succsessfully"})
